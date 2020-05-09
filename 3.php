b3d82d93d14e999b0e929de4263f5abe1259c508
byasaa <abi.vardhana@outlook.com>
2020-05-09 19:13:16 +0700

    fix number 3

--- 3.php
+++ 3.php
@@ -2,20 +2,7 @@
 
 function count_vowels($word)
 {
-    $arr = str_split($word);
-    $vocal = ['a','i','u','e','o'];
-
-    $found = array_intersect($vocal, $arr);
-
-    echo $found;
-    // $count = array_count_values($arr);
-
-    // $result = [];
-
-    // foreach ($found as $item) {
-    //     $result[$item] = $count[$item];
-    // }
-    // return $result;
+    echo preg_match_all('/[aeiou]/i',$word,$matches);
 }
 
-var_dump(count_vowels('aiu'));
+echo (count_vowels('aiu'));
