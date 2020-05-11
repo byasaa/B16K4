4854b569bfe9b8973c1806505ac7ddb85edeb355
byasaa <63111238+byasaa@users.noreply.github.com>
2020-05-11 20:36:06 +0700

    fixed

--- 5.php
+++ 5.php
@@ -1,15 +1,23 @@
 <?php
-function createTriangle($a,$b){
-    for ($a; $a<=$b; $a++) {
-        echo "  ".$a;
-        for ($a; $a<=$b; $a++){
-            echo $a+$a;
-            for ($b; $b>= $a; $b--){
-                echo $b;
+// un = a + (n-1)b
+#
+// a := suku pertama b:= beda n:=0,1,2
+// b = un=u^n-1; u1(2) - u1
+
+function createTriangle($a,$u){
+    if($u%$a == 0){
+        $baris = $u/$a;
+        $beda = $a*2 - $a;
+        for($i = 1; $i <= $baris; $i++){
+            for($j = $baris; $j > $i; $j--){
+                echo " ";
             }
+            for($n=1; $n<=$i; $n++){
+                $un = $a + ($n-1)*$beda;
+                echo $un;
+            }
+            echo "\n";
         }
-        echo '<br>';
     }
 }
 echo createTriangle(2,10);
-?>
